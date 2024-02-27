7bb17637f55e2d7f47269ef1e6bc35e3e1840988
WrightCD <carldonovanwright@gmail.com>
2024-02-27 17:45:34 -0500

    Initial Commit

---
+++ TaskTrailUITests/TaskTrailUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  TaskTrailUITestsLaunchTests.swift
+//  TaskTrailUITests
+//
+//  Created by Carl Wright on 2024-02-27.
+//
+
+import XCTest
+
+final class TaskTrailUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
