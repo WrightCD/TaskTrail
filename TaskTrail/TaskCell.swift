1b97c734829677c47d9974dd411965eefda4ddfa
WrightCD <carldonovanwright@gmail.com>
2024-03-15 22:47:26 -0400

    Intitial

---
+++ TaskTrail/TaskCell.swift
@@ -0,0 +1,15 @@
+//
+//  TaskCell.swift
+//  TaskTrail
+//
+//  Created by Carl Wright on 2024-02-27.
+//
+
+import Foundation
+import UIKit
+
+class TaskCell : UITableViewCell{
+    
+    
+    
+}
