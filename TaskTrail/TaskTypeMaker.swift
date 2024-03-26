928a436139a23a396f357f96b0134b1dd59f73e5
WrightCD <carldonovanwright@gmail.com>
2024-03-25 22:15:57 -0400

    Update TrailTask & TypeMaker

---
+++ TaskTrail/TaskTypeMaker.swift
@@ -0,0 +1,29 @@
+//
+//  TaskTypeMaker.swift
+//  TaskTrail
+//
+//  Created by Carl Wright on 2024-03-25.
+//
+
+import UIKit
+
+class TaskTypeMaker: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
